c1434b0c84c6941f04176bd91e0c9a0b6f1e356b
Mizanur Rahman <mizan.prog@gmail.com>
2023-06-12 22:12:27 +0600

    printing linked-list

---
+++ 4.LinkedList/SingleLinkedList.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+        int val;
+        Node* next;
+        Node(int val){
+            this->val=val;
+            this->next=NULL;
+        }
+};
+int main(){
+   Node *head=new Node(12);
+   Node *a=new Node(24);
+   Node *b=new Node(45);
+   Node *c=new Node(54);
+   Node *d=new Node(78);
+   head->next=a;
+   a->next=b;
+   b->next=c;
+   c->next=d;
+   while (head !=NULL){ 
+    cout << head->val << endl;
+    head=head->next;
+   
+   }
+   return 0;
+}
